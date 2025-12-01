98ad240dd9c65401d5dc01dc14d89835868b3759
Adrian Chipana C. <adrianchipana1@gmail.com>
2025-12-01 01:01:34 -0500

    Versión final del código

--- Evaluador_Prefijo.cpp
+++ Evaluador_Prefijo.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<sstream>
+#include<fstream>
 
 using namespace std;
 
@@ -32,7 +33,6 @@ struct Pila {
         Nodo *nuevo = new Nodo(val);
         nuevo->sig = puntero;
         puntero = nuevo;
-        cout<<"Apilamos el operando "<<val<<endl<<endl;
         tam++;
     }
 
@@ -43,20 +43,29 @@ struct Pila {
             return;
         }
         Nodo *temp = puntero;
-        double valor = puntero->var;
         puntero = puntero->sig;
         delete temp;
-        cout<<"Desapilamos el valor "<<valor<<endl<<endl;
         tam--;
     }
 
     // Obtenemos el últimor valor apilado. Si no hay, salimos del programa, faltan operandos.
     double verTope() {
         if (puntero == nullptr) {
-            cout << "Faltan operandos. La cadena no es aceptada.\n";
             exit(1);
         }
         return puntero->var;  
+    }
+
+    string verPila(){
+        string lista;
+        if(tam == 0){
+            return "";
+        }
+        for(int i = 0; i < tam; i++) {
+            lista += "X";
+        }
+        lista += "Z";
+        return lista;
     }
 
 };
@@ -64,35 +73,41 @@ struct Pila {
 int main() {
 
     Pila  *pila = new Pila();
-    string entrada;             // String para ingresar la cadena, divida por espacios
+    string token;             // String para ingresar la cadena, divida por espacios
     string cadena[100];         // Donde registraremos cada elemento de la cadena
-    int indice = 0;             
+    int indice = -1;             
 
-    cout<<"------------------------------------------------------------\n";
-    cout<<"-------- Evaluador y validador Prefijo con Autómata --------\n";
-    cout<<"------------------------------------------------------------\n\n";
+    ifstream entrada("documento.txt");
+    ofstream pasos("resultado.txt", ios::out | ios::trunc);
 
-    cout<<"Ingrese la cadena a evaluar, separe los operandos y operadores por espacios: \n";
-    getline(cin, entrada);
-    cout<<endl<<endl;
-
-    // Separamos los simbolos de la cadena
-    stringstream elementos(entrada);
-
-    // Los insertamos en un array para analizar simbolo
-    while (elementos >> cadena[indice] && indice < 100) {
+    do {
         indice++;
-    }
+        getline(entrada, cadena[indice]);
+        if(indice == 100) {
+            pasos<<"Cadena sobrepadasa, inserte una pila más corta.";
+            return 1;
+        }
+    } while (cadena[indice].length() > 0);
+
 
     // El bucle lee de derecha a izquierda, como se requiere para el orden prefijo
     for(int i = indice - 1; i >= 0; i--) {
 
         double num;             
-        entrada = cadena[i];
+        token = cadena[i];
 
         // Comprobamos que sea un operando y si lo es lo apilamos
-        if(isdigit(entrada[0])) {
-            pila->apilar(stod(entrada));
+        if(isdigit(token[0])) {
+            pila->apilar(stod(token));
+
+            if(i == indice - 1) {
+                pasos<<token<<"\tZ\tq0\t\t"<<"q1\tXZ\t"<<pila->verPila();
+            }
+            else {
+                pasos<<token<<"\tX\tq1\t\t"<<"q1\tXX\t"<<pila->verPila();
+            }
+            pila->verPila();
+            pasos<<endl;
         }
         // Si no es un operando, analizamos que operador es. En cada caso se aplica la operación y se inserta en la pila
         else {
@@ -101,22 +116,22 @@ int main() {
             double operando2 = pila->verTope();
             pila->desapilar();
 
-            if(entrada == "+") {
+            if(token == "+") {
                 num = operando1 + operando2;
                 pila->apilar(num);
             }
-            else if(entrada == "-") {
+            else if(token == "-") {
                 num = operando1 - operando2;
                 pila->apilar(num);
             }
-            else if(entrada == "/") {
+            else if(token == "/") {
                 if(operando2 == 0) {
                     cout<<"Operanción inválida, no se puede dividir entre 0. El resultado es indefinido.";
                     exit(1);
                 }
                 num = operando1 / operando2;
                 pila->apilar(num);
-            }else if(entrada == "*") {
+            }else if(token == "*") {
                 num = operando1 * operando2;
                 pila->apilar(num);
             }
@@ -125,19 +140,24 @@ int main() {
                 cout<<"Simbolo no válido. La cadena no es aceptada.";
                 return 1;
             }
+            pasos<<token<<"\tXX\tq1\t\t"<<"q1\tX\t"<<pila->verPila();
+            pila->verPila();
+            pasos<<endl;
         }
     }
     // Evaluamos casos de la pila vacía o si sobran operandos
     if(pila->tam == 0) {
-        cout<<"Cadena vacía. No se ha procesado ninguna opración.";
+        cout<<"Cadena vacía. No se ha procesado ninguna operación.";
     }
     else if(pila->tam > 1) {
         cout<<"Sobrando operandos. La cadena no es válida";
     }
     // Si no hay errores, imprimimos el resultado
     else {
-        cout<<"La cadena es aceptada.\n";
-        cout<<"El resultado de la pila es: "<<pila->verTope();
+        pasos<<"λ\tXZ\tq1\t\t"<<"q2\tZ\tZ"<<endl;
+        pasos<<"λ\tZ\tq2\t\t"<<"q2\tλ\tλ"<<endl;
+        pasos<<pila->verTope();
+        pila->desapilar();
     }
     return 0;
 }
